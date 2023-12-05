cbaf4a9525e93c062a9bccd315e88ac4f742687d
Tor S. Haugland <torshaugland@gmail.com>
2023-12-05 20:36:34 +0100

    Update main.jl

--- 5/main.jl
+++ 5/main.jl
@@ -1,15 +1,18 @@
-sing DataStructures
+using DataStructures
 
-struct SeedMap
-    srcs # Range of src
-    dests # Range of dest
-end
-
-function get_seed(map :: SeedMap, src, default)
-    for (map_src, map_dest) in zip(map.srcs, map.dests)
-        if map_src[1] <= src <= map_src[2]
-            i = src - map_src[1] + 1
-            val = map_dest[i]
+function get_seed(srcs, dests, s, default = s)
+    #for (map_src, map_dest) in zip(map.srcs, map.dests)
+        #if map_src[1] <= src <= map_src[2]
+    
+    n = length(srcs)
+    j = 0
+    while j < n
+        j += 1
+        a = srcs[j][1]
+        b = srcs[j][end]
+        if a <= s <= b
+            i = s - srcs[j][1] + 1
+            val = dests[j][i]
             return val
         end
     end
@@ -18,8 +21,8 @@ end
 
 function parse_map(io)
     str = parse.(Int, split(readline(io)))
-    srcs = StepRange{Int,Int}[]
-    dests = StepRange{Int,Int}[]
+    srcs = UnitRange{Int}[]
+    dests = UnitRange{Int}[]
     while !isempty(str)
         dest = str[1]
         src = str[2]
@@ -30,90 +33,58 @@ function parse_map(io)
 
         str = parse.(Int, split(readline(io)))
     end
-    return SeedMap(srcs, dests)
+    return srcs, dests
 end
 
 function parse_input()
     open("input", "r") do io
         line = readline(io)
-        global seeds = parse.(Int, split(line)[2:end])
+        seeds = parse.(Int, split(line)[2:end])
         _ = readline(io)
         _ = readline(io)
-        map1 = parse_map(io)
-        global maps = [map1]
+        src, dest = parse_map(io)
+        srcs = [src]
+        dests = [dest]
         for _ = 2:7
             _ = readline(io)
-            map = parse_map(io)
-            push!(maps, map)
+            src, dest = parse_map(io)
+            push!(srcs, src)
+            push!(dests, dest)
         end
+        return seeds, srcs, dests
     end
-    return seeds, maps
-end
-
-function merge_map(maps)
-    src = values(maps[7])
-    for i = 7:1
-        key = keys(maps[i])
-        val = values(maps[i])
-        y = Int[]
-        for x in src
-            if x in val
-                push!(y, key[val == x])
-            end
-        end
-        x = copy(y)
-    end
-
-    loc_map = OrderedDict{Int,Int}()
-    loc_map[]
-
-    return loc_map
-end
-
-function find_seed(src, map)
-    x = -1
-    for i = 0:src
-        x += 1
-        while (x in keys(map))
-            x += 1
-        end
-    end
-    return x
 end
 
 function part1()
-    seeds, maps = parse_input()
+    seeds, srcs, dests = parse_input()
     score = typemax(Int)
     for seed in seeds
-        src = seed
-        for map in maps
-            src = get_seed(map, src, src)
+        s = seed
+        for (src, dest) in zip(srcs, dests)
+            s = get_seed(src, dest, s)
         end
-        score = min(score, src)
+        score = min(score, s)
     end
     score
 end
 
 function part2()
-    seeds, maps = parse_input()
+    seeds, srcs, dests = parse_input()
     scores = Int[]
-    for i = 1:(length(seeds)÷2)
+    Threads.@threads for i = 1:(length(seeds)÷2)
         score = typemax(Int)
-        @show i
-        for seed in seeds[2i-1]:(seeds[2i-1]+seeds[2i])
-        #for seed in seeds[2i-1]:(seeds[2i-1])#+seeds[2i])
-        #for seed in (seeds[2i-1]+seeds[2i]):(seeds[2i-1]+seeds[2i])
-            src = seed
-            for map in maps
-                src = get_seed(map, src, src)
+        for seed in seeds[2i-1]:(seeds[2i-1]+seeds[2i]-1)
+            s = seed
+            for (src, dest) in zip(srcs, dests)
+                s = get_seed(src, dest, s)
             end
-            score = min(score, src)
+            score = min(score, s)
         end
+        @show score
         push!(scores, score)
     end
-    @show scores
     minimum(scores)
 end
 
 @show part1()
-@show part2()
+@show part2() # answer -1 on input file. why?
