c74b5f86ac253c752d4ab115e00d8a491ed94687
Tor S. Haugland <torshaugland@gmail.com>
2023-12-11 20:21:45 +0100

    Improve day 1 with chatgpt

--- 1/main.jl
+++ 1/main.jl
@@ -1,60 +1,50 @@
-x = []
-int0 = 0
-int1 = 0
-s = 0
-for line in readlines("input")
-    first = true
-    print(line)
-    for c in line
+const wordMap = Dict("one" => 1, "two" => 2, "three" => 3, "four" => 4, 
+                     "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, 
+                     "nine" => 9, "zero" => 0)
 
-        try
-            if (first)
-                int0 = parse(Int, c)
-                first = false
-            end
-            int1 = parse(Int, c)
-        catch
-            continue
-        end
-    end
-    s += 10*int0 + int1
-    @show int0, int1
-    @show s
-end
-@show s
+function extractFirstAndLastNumbers(line, useWords=false)
+    firstNum = -1
+    lastNum = -1
+    wordMap = Dict("one" => 1, "two" => 2, "three" => 3, "four" => 4, 
+                   "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, 
+                   "nine" => 9, "zero" => 0)
 
-x = []
-int0 = 0
-int1 = 0
-s = 0
-num = Dict("one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9, "zero" => 0)
-for line in readlines("input")
-    first = true
-    print(line)
-
-    for (i, c) in enumerate(line)
-        for k in keys(num)
-            if startswith(line[i:end], k)
-                if (first)
-                    int0 = num[k]
-                    first = false
+    for (index, char) in enumerate(line)
+        if isdigit(char)
+            num = parse(Int, char)
+            firstNum = (firstNum == -1) ? num : firstNum
+            lastNum = num
+        elseif useWords && isletter(char)
+            for (word, number) in wordMap
+                if startswith(line[index:end], word)
+                    firstNum = (firstNum == -1) ? number : firstNum
+                    lastNum = number
+                    break  # Exit the loop as we found a valid number word
                 end
-                int1 = num[k]
             end
-        end
-
-        try
-            if (first)
-                int0 = parse(Int, c)
-                first = false
-            end
-            int1 = parse(Int, c)
-        catch
-            continue
         end
     end
-    s += 10*int0 + int1
-    @show int0, int1
-    @show s
+
+    return firstNum, lastNum
 end
-@show s
+
+function partOneCalibration()
+    totalSum = 0
+    for line in readlines("1/input")
+        firstNum, lastNum = extractFirstAndLastNumbers(line)
+        totalSum += 10 * firstNum + lastNum
+    end
+    return totalSum
+end
+
+function partTwoCalibration()
+    totalSum = 0
+    for line in readlines("1/input")
+        firstNum, lastNum = extractFirstAndLastNumbers(line, true)
+        totalSum += 10 * firstNum + lastNum
+    end
+    return totalSum
+end
+
+println("Part One Total Sum: ", partOneCalibration())
+println("Part Two Total Sum: ", partTwoCalibration())
